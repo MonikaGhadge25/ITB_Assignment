89b6d94ae82c9d771a3ef65add68fa495a6c0dc2
MonikaGhadge25 <monikaghadage25@gmail.com>
2025-03-05 10:33:52 +0530

    done

--- Assignment1/src/app/app.routes.ts
+++ Assignment1/src/app/app.routes.ts
@@ -1,3 +1,30 @@
-import { Routes } from '@angular/router';
+// import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// export const routes: Routes = [];
+
+import { Routes } from '@angular/router';
+import { LoginComponent } from './auth-user/login/login.component';
+import { RegisterComponent } from './auth-user/register/register.component';
+import { UserComponent } from './auth-user/user/user.component';
+import { ForgetpasswordComponent } from './auth-user/forgetpassword/forgetpassword.component';
+
+export const routes: Routes = [
+
+    {
+        path : 'login',
+        component : LoginComponent
+    },
+    {
+        path : 'register',
+        component : RegisterComponent
+    },
+    {
+        path : 'user',
+        component : UserComponent
+    },
+    {
+        path : 'forgetpassword',
+        component : ForgetpasswordComponent
+    }
+
+];
